b953ae850dd59067771d555827e637011ba3b27d
Anand Muraleedharan <anand.muraleedharan@gmail.com>
2019-01-22 12:24:33 -0600

    Create reverseWords.js

---
+++ reverseWords.js
@@ -0,0 +1,20 @@
+//Algorithm: Reverse Words - Without Array built-in function reverse()
+//Backward for Loop
+
+function reverseWords(string) {
+  var wordsArr = string.split(' ');
+  var reversedWordsArr = [];
+  
+  wordsArr.forEach(word => {
+    var reversedWord = '';
+    for (var i = word.length - 1; i >= 0; i--) {
+      reversedWord += word[i];
+    };
+    reversedWordsArr.push(reversedWord);
+  });
+  
+  return reversedWordsArr.join(' ');
+}
+
+//Testing
+reverseWords('Coding JavaScript');
